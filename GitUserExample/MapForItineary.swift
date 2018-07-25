2f1da5d16df0ddc7e21eb209922d0af1b1ff0362
IRMEDIA <wescravn@gmail.com>
2018-07-25 06:56:23 -0400

    new map feature implementation

---
+++ GitUserExample/MapForItineary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItineary.swift
+//  GitUserExample
+//
+//  Created by Tech2KNY on 7/25/18.
+//  Copyright © 2018 Internet Records. All rights reserved.
+//
+
+import UIKit
+
+class MapForItineary: NSObject {
+
+}
